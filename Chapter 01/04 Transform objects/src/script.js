8781010e3970e12cd956696883cee2779e0ffb0b
Xxuu9 <tengxxu@gmail.com>
2024-04-03 23:38:18 -0400

    initial commit

---
+++ Chapter 01/04 Transform objects/src/script.js
@@ -0,0 +1,86 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// add Axes right after the scene
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+// mesh.position.set(0.7, -0.6, 1);
+// mesh.scale.x = 0.5;
+// mesh.scale.y = 0.25;
+// mesh.scale.z = 0.5;
+
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+// Object.rotation.reorder("YZX");
+// scene.add(mesh);
+
+//gives you the distance of the mesh from the origin of the coordinate system.
+// console.log(mesh.position.length());
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+camera.lookAt(new THREE.Vector3(1, -2, 0));
+//gives you the distance between the mesh and the camera.
+// console.log(mesh.position.distanceTo(camera.position));
+// console.log(mesh.position.normalize());
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
